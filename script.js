eed671a0b5ad913ca6aa089cd4cbd82171dcfa8a
ghoudriamine-tech <ghoudriamine@gmail.com>
2026-08-23 23:28:19 +0100

    Change Supabase key to a new value
    Updated Supabase key for security reasons.

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const SUPABASE_URL =
   "https://rbmttbxsezttysenbcwn.supabase.co";
 
 const SUPABASE_KEY =
-  "sb_publishable_tVChEdNRQHs9lrYPjA4ajQ_heTXT2x2";
-
+  "";
+sb_publishable_tVChEdNRQHs9lrYPjA4ajQ_heTXT2xw
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
