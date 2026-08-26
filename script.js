46e2fe73f30df8d5657fc2842f04537e1acf2b14
ghoudriamine-tech <ghoudriamine@gmail.com>
2026-08-26 11:56:13 +0100

    تحديث تصميم الصفحة الرئيسية

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const SUPABASE_URL =
   "https://rbmttbxsezttysenbcwn.supabase.co";
 
 const SUPABASE_KEY =
-  "sb_publishable_tVChEdNRQHs9lrYPjA4ajQ_heTXT2x2";
+  "sb_publishable_tVChEdNRQHs9lrYPjA4ajQ_heTXT2xw";
 
 
 async function loadAccommodations() {
